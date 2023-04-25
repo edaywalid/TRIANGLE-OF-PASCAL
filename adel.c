0a9714e037815fa90edd133d6af315475e73eab5
Walid <becharwalid9@gmail.com>
2023-04-25 21:44:17 +0100

    changes

--- adel.c
+++ adel.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct node* Liste;
+#include <stdbool.h>
+typedef struct node *Liste;
 typedef struct node
 {
     int data;
     Liste next;
 
-}node;
+} node;
 
+void print(Liste head)
+{
 
-void print (Liste head){
-
-    while (head != NULL){
-        printf("%d  ",head->data);
+    while (head != NULL)
+    {
+        printf("%d  ", head->data);
         head = head->next;
     }
 }
-Liste pascal(Liste tete, Liste tete2){
+Liste pascal(Liste tete, Liste tete2)
+{
 
     Liste p = tete2;
     while (tete->next != NULL)
@@ -27,40 +30,107 @@ Liste pascal(Liste tete, Liste tete2){
         tete = tete->next;
     }
     p->next = malloc(sizeof(node));
-    p->next->data= 1;
+    p->next->data = 1;
     return tete2;
 }
 
+void F(Liste T[], int N, int nb)
+{
+    int i = 0;
+    bool found = false;
+    if (nb == 1)
+    {
+        i = 0;
+    }
+    else
+    {
+        i = 1;
+        while (i < N && !found)
+        {
+            Liste p = T[i];
+            while (p != NULL)
+            {
+                if (p->data < nb)
+                {
+                    p = p->next;
+                }
+                else
+                {
+                    found = true;
+                    break;
+                }
+            }
+            T[i] = pascal(T[i], T[i + 1]);
+            i++;
+        }
+    }
 
-    
-int main(){
-    Liste T[100];
-    for (int i = 0; i < 100; i++)
+    for (int j = 0; j < i - 1; j++)
+    {
+        print(T[j]);
+        pascal(T[j], T[j + 1]);
+        printf("\n\n");
+    }
+}
+
+void Liberer(Liste T[], int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        Liste p = T[i];
+        while (p != NULL)
+        {
+            Liste temp = p;
+            p = p->next;
+            free(temp);
+        }
+    }
+}
+
+void construire(Liste T[], int nbLIGNE)
+{
+    for (int i = 0; i < nbLIGNE; i++)
+    {
+        print(T[i]);
+        pascal(T[i], T[i + 1]);
+        printf("\n\n");
+    }
+}
+int main()
+{
+    int N;
+    printf("enter the number of lines :");
+    scanf("%d", &N);
+    Liste T[N + 1];
+    for (int i = 0; i < N + 1; i++)
     {
         T[i] = NULL;
     }
-    
-    for (int i = 0; i < 100; i++)
-    {  
-        T[i] = malloc ( sizeof (node));
-        T[i]->data = 1;
-        T[i]->next= NULL;
-    }
-    int N ;
-    printf("enter how many lines u want to get :");
-    scanf("%d",&N);
 
-    printf("1\n");
-    for (int  i = 0; i < N; i++)
+    for (int i = 0; i < N + 1; i++)
     {
-        print(pascal(T[i],T[i+1]));
-        printf("\n\n\n");
-
+        T[i] = malloc(sizeof(node));
+        T[i]->data = 1;
+        T[i]->next = NULL;
     }
     
     
-
-
+    printf("\n\nyour whole triangle :\n\n");
+    construire(T, N);
+    Liberer(T, N);
+    for (int i = 0; i < N + 1; i++)
+    {
+        T[i] = malloc(sizeof(node));
+        T[i]->data = 1;
+        T[i]->next = NULL;
+    }
     
+    int X;
+    printf("which number u want to stop in :");
+    scanf("%d", &X);
+    F(T, N, X);
+
+    Liberer(T, N);
+
+    return 0;
 }
-    
